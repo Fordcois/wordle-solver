d0b6c917c8e60fa887054488f3b6dc6396443f63
Sam Ford <samueljamesford@googlemail.com>
2024-07-30 19:06:29 +0100

    Animates Banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+'use client'
+import React, { useEffect, useState } from "react";
 
-
+const colors = ['var(--Grey)', 'var(--Yellow)', 'var(--Green)'];
 
 const Header: React.FC = () => {
+  const [letters, setLetters] = useState([
+    { char: 'S', color: 'var(--Green)' },
+    { char: 'o', color: 'var(--Grey)' },
+    { char: 'l', color: 'var(--Grey)' },
+    { char: 'v', color: 'var(--Yellow)' },
+    { char: 'r', color: 'var(--Yellow)' },
+  ]);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLetters(prevLetters => {
+        const index = Math.floor(Math.random() * prevLetters.length);
+        const newColor = colors[Math.floor(Math.random() * colors.length)];
+        const newLetters = [...prevLetters];
+        newLetters[index] = { ...newLetters[index], color: newColor };
+        return newLetters;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="Title">
-        Solvr
+      {letters.map((letter, index) => (
+        <span key={index} style={{ color: letter.color }}>{letter.char}</span>
+      ))}
     </div>
   );
 };
