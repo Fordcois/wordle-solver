5af80de67bb84d99f96ccbece7d9880ace3a7b93
Sam Ford <samueljamesford@googlemail.com>
2024-08-06 16:58:25 +0100

    Install Vercel Analytics/SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Solvr: Wordle Solver",
   description: "Struggle with Wordle? We've got you covered!",
+  keywords: ['Wordle Solver','Wordle Solution','Wordle'],
+  openGraph: {
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({
@@ -17,6 +24,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
